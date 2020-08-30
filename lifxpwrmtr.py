1d78e53751e41bd81c618b06611b7bffe908484b
Joe Lipson <joe.lipson@abc.net.au>
2020-08-30 22:44:27 +1000

    got classes working

--- lifxpwrmtr.py
+++ lifxpwrmtr.py
@@ -1,17 +1,23 @@
-
+import random
 from lifxlan import Light
 
-class Lifxpwrmtr(Light):
+class Light(Light):
 
+  #def __init__(self, mac_addr, ip_addr, service=1, port=56700, source_id=random.randrange(2, 1 << 32), verbose=False):
+  #  super().__init__(mac_addr, ip_addr, service, port, source_id, verbose)
 
   def flame_on(self):
     print('flame_on')
 
-  def snap_freeze(self):
+  def blizzard(self):
     print('snap_freeze')
 
   def set_heat(self, intensity, magnitude):
-    print('set_heat')
+    print(f"set_heat {intensity} : {magnitude}")
+    # yellow - orange - red - flames!
+    # hue 64 -- 0
 
   def set_cool(self, intensity, magnitude):
     print('set_cool')
+    # green - cyan - blue - purple/blue
+    # hue 128 -- 256
--- solar_globe.py
+++ solar_globe.py
@@ -3,41 +3,54 @@
 import os
 import solaredge
 import lifxpwrmtr
-from lifxpwrmtr import set_heat
 
+# solaredge site ID
 site_id = 1577716
+
+# max power difference
+# value in kW that is considered max in terms of the colour scales
+# i.e if it's set to 4 then drawing 4kW from the grid is the top
+# of the scale, anything above that makes the light flame.
+# Likewise if we're putting 4kW from PV back into the grid then
+# that is considered max, anything above that is blizzard! 
+max_pwr_diff = 4
+
+# Value in kW that is considered the maximum power we draw in total
+# This value is used to set the brightness. e.g. if this is set to
+# 8 then when we're drawing 8kW or above the glove is at max brightness
+max_pwr_total = 8
+
 s = solaredge.Solaredge(os.environ['SOLAREDGE_API_KEY'])
 
 data = s.get_current_power_flow(site_id)
 pwr_total     = data['siteCurrentPowerFlow']['LOAD']['currentPower']
 pwr_from_grid = data['siteCurrentPowerFlow']['GRID']['currentPower']
 pwr_from_pv   = data['siteCurrentPowerFlow']['PV']['currentPower']
+pwr_to_grid   = max(0, pwr_from_pv - pwr_total)
 
 
 light = lifxpwrmtr.Light("12:34:56:78:9a:bc", "192.168.1.42")
 
-hue = 0
-saturation = 65535
 #pwr_total sets brightness
-brightness = pwr_total/8 * 65535
-kelvin = 0
+brightness = pwr_total/max_pwr_total * 100
 
 if pwr_from_grid > 0:
-  if pwr_from_grid > 4:
+  intensity = min(100, pwr_from_grid / max_pwr_diff * 100)
+
+  if pwr_from_grid > max_pwr_diff:
     light.flame_on()
   else:
-    light.set_heat(pwr_from_grid, pwr_total)
- # yellow - orange - red - flames!
- # hue 64 -- 0
+    light.set_heat(intensity, brightness)
 else:
-  if pwr_from_pv > 4:
-    light.snap_freeze()
+  intensity = min(100, pwr_to_grid / max_pwr_diff * 100)
+
+  if pwr_to_grid > max_pwr_diff:
+    light.blizzard()
   else:
-    light.set_cool(pwr_from_pv, pwr_total)
- # green - cyan - blue - purple/blue
- # hue 128 -- 256
+    light.set_cool(intensity, brightness)
 
 print(f"total power:{pwr_total}")
 print(f"power from grid:{pwr_from_grid}")
 print(f"power from pv:{pwr_from_pv}")
+print(f"power to grid:{pwr_to_grid}")
 
