cfd28d1293107cc2e4ce2ee25b15330e16b225a8
Joe Lipson <joe.lipson@abc.net.au>
2020-09-01 13:34:39 +1000

    add flow logic and if hue bug

--- lifxpwrmtr.py
+++ lifxpwrmtr.py
@@ -1,6 +1,8 @@
 import random
 from lifxlan import Light
 
+kelvin = 6500
+
 class Light(Light):
 
   #def __init__(self, mac_addr, ip_addr, service=1, port=56700, source_id=random.randrange(2, 1 << 32), verbose=False):
@@ -14,10 +16,24 @@ class Light(Light):
 
   def set_heat(self, intensity, magnitude):
     print(f"set_heat {intensity} : {magnitude}")
+
+    hue = ((64 - (intensity * (64/100)))/360) * 65535
+    saturation = 65535
+    brightness = 65535 * (magnitude/100)
+    colour = [hue, saturation, brightness, kelvin]
+    print(colour)
+    self.set_color(colour, 3000)
     # yellow - orange - red - flames!
     # hue 64 -- 0
 
   def set_cool(self, intensity, magnitude):
-    print('set_cool')
+    print(f"set_cool {intensity} : {magnitude}")
+
+    hue = ((128 + (intensity * 128/100))/360) * 65535
+    saturation = 65535
+    brightness = 65535 * (magnitude/100)
+    colour = [hue, saturation, brightness, kelvin]
+    print(colour)
+    self.set_color(colour, 3000)
     # green - cyan - blue - purple/blue
     # hue 128 -- 256
--- solar_globe.py
+++ solar_globe.py
@@ -17,17 +17,29 @@ max_pwr_diff = 4
 
 # Value in kW that is considered the maximum power we draw in total
 # This value is used to set the brightness. e.g. if this is set to
-# 8 then when we're drawing 8kW or above the glove is at max brightness
+# 8 then when we're drawing 8kW or above the globe is at max brightness
 max_pwr_total = 8
 
 s = solaredge.Solaredge(os.environ['SOLAREDGE_API_KEY'])
 
 data = s.get_current_power_flow(site_id)
-pwr_total     = data['siteCurrentPowerFlow']['LOAD']['currentPower']
-pwr_from_grid = data['siteCurrentPowerFlow']['GRID']['currentPower']
-pwr_from_pv   = data['siteCurrentPowerFlow']['PV']['currentPower']
-pwr_to_grid   = max(0, pwr_from_pv - pwr_total)
+#print(data)
+for c in data['siteCurrentPowerFlow']['connections']:
+  if c['from'] == 'LOAD' and c['to'] == 'Grid':
+    pwr_from_grid = 0 
+    pwr_to_grid = data['siteCurrentPowerFlow']['GRID']['currentPower']
 
+  if c['from'] == 'Grid' and c['to'] == 'LOAD':
+    pwr_from_grid = data['siteCurrentPowerFlow']['GRID']['currentPower']
+    pwr_to_grid = 0
+
+pwr_total     = data['siteCurrentPowerFlow']['LOAD']['currentPower']
+pwr_from_pv   = data['siteCurrentPowerFlow']['PV']['currentPower']
+
+print(f"total power:{pwr_total}")
+print(f"power from grid:{pwr_from_grid}")
+print(f"power from pv:{pwr_from_pv}")
+print(f"power to grid:{pwr_to_grid}")
 
 light = lifxpwrmtr.Light("12:34:56:78:9a:bc", "192.168.1.42")
 
@@ -41,6 +53,7 @@ if pwr_from_grid > 0:
     light.flame_on()
   else:
     light.set_heat(intensity, brightness)
+
 else:
   intensity = min(100, pwr_to_grid / max_pwr_diff * 100)
 
@@ -48,9 +61,4 @@ else:
     light.blizzard()
   else:
     light.set_cool(intensity, brightness)
-
-print(f"total power:{pwr_total}")
-print(f"power from grid:{pwr_from_grid}")
-print(f"power from pv:{pwr_from_pv}")
-print(f"power to grid:{pwr_to_grid}")
 
